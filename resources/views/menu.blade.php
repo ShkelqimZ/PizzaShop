e32cf977865af7f43b0fdd359da61d60086b67b2
PC <shkelqim.zahiti@gmail.com>
2018-07-31 07:56:40 +0200

    edit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -13,14 +13,12 @@
                                 <h2 class="text-center">{{$product->title}}</h2>
                                 <p class="productDesc">{{$product->description}}</p>
                             </div>
-                            <a href="/menu/{{$product->product_id}}">
-                                <div class="cover" id="coverDiv-{{$product->product_id}}">
-                                    @if(Auth::check() && Auth::user()->type == 'admin')
-                                    <a href="/editProduct/{{$product->product_id}}"><i class="fa fa-edit"></a></i>
-                                    @endif
-                                    <i class="fa fa-shopping-basket"></i>
-                                </div>
-                            </a>
+                            <div class="cover" id="coverDiv-{{$product->product_id}}">
+                                @if(Auth::check() && Auth::user()->type == 'admin')
+                                <a href="/editProduct/{{$product->product_id}}"><i class="fa fa-edit"></i></a>
+                                @endif
+                                <a href="/menu/{{$product->product_id}}"><i class="fa fa-shopping-basket"></i></a>
+                            </div>
                         </div>
                     </div>
                     @endforeach
